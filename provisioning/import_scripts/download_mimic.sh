c21135746932c29685e48c96649c24473fd856c1
Nikhil Haas <nikhil@nikhilhaas.com>
2016-01-05 14:44:16 -0800

    Add shell script to download MIMIC-III data from PhysioNet

---
+++ provisioning/import_scripts/download_mimic.sh
@@ -0,0 +1,43 @@
+jobsrunning=0
+maxjobs=9
+
+for ITEM in ADMISSIONS \
+            CALLOUT \
+            CAREGIVERS \
+            CHARTEVENTS \
+            CPTEVENTS \
+            DATETIMEEVENTS \
+            DIAGNOSES_ICD \
+            DRGCODES \
+            D_CPT \
+            D_ICD_DIAGNOSES \
+            D_ICD_PROCEDURES \
+            D_ITEMS \
+            D_LABITEMS \
+            ICUSTAYS \
+            INPUTEVENTS_CV \
+            INPUTEVENTS_MV \
+            LABEVENTS \
+            MICROBIOLOGYEVENTS \
+            NOTEEVENTS \
+            OUTPUTEVENTS \
+            PATIENTS \
+            PRESCRIPTIONS \
+            PROCEDUREEVENTS_MV \
+            PROCEDURES_ICD \
+            SERVICES \
+            TRANSFERS
+do
+    if [ $jobsrunning -eq $maxjobs ]; then
+        jobsrunning=0
+        wait
+    fi
+    jobsrunning=$(( $jobsrunning+1))
+    (
+    TAR_DIR=/home/vagrant/src/mimic-iii/tarballs
+    DEST=$TAR_DIR/$ITEM.tar.gz 
+    BASE_URL=https://physionet.org/works/MIMICIIIClinicalDatabase/files
+    URL=$BASE_URL/version_1_3/$ITEM.csv.gz
+    wget --user $1 --password $2 --continue --no-check-certificate -O $DEST $URL
+    ) &
+done
